10caedec9d3457b819fcd6676b22a79b14415c33
Deac <139060984+DeactivatedMan@users.noreply.github.com>
2025-09-06 17:37:42 +0000

    Feat drop non-ores

--- startup.lua
+++ startup.lua
@@ -5,10 +5,12 @@ local spacing = tonumber(read())+1
 print("How many tunnels should I make?")
 local tunnels = tonumber(read())
 print("What direction should I move towards?\n[Left / Right]")
-local temp = string.lower(tostring(read()))
+local temp = tostring(read()).lower()
 local direct = string.find(temp, "l") and 1 or 0
---[[print("What do I leave?\n Examples: stone,deepslate,andesite")
-local listed = string.gsub(read(), "%s", "")
+print("Do I drop everything other than ores?")
+local temp = tostring(read()).lower()
+local drop = string.find(temp, "y") and 1 or 0
+--[[local listed = string.gsub(read(), "%s", "")
 local leave = {}
 
 for word in string.gmatch(listed, "[^,]+") do
@@ -26,6 +28,7 @@ for tunnel=1,tunnels do
 
         while turtle.getFuelLevel() <= 0 do
             term.clear()
+            term.setCursorPos(0,0)
             term.write("Out of fuel!")
             for s=1,16 do
                 if turtle.getFuelLevel() <= 0 then
@@ -34,26 +37,28 @@ for tunnel=1,tunnels do
                 end
             end
             sleep(2)
+            term.clear()
         end
     end
-    if direct then turtle.turnLeft() else turtle.turnRight() end
+    if direct==1 then turtle.turnLeft() else turtle.turnRight() end
     for _=1,spacing do turtle.dig();turtle.forward();turtle.digDown() end
-    if direct then turtle.turnLeft() else turtle.turnRight() end
+    if direct==1 then turtle.turnLeft() else turtle.turnRight() end
 
     direct = direct * -1
-    --[[term.clear()
-    term.write("Dumping bad items!")
+    if drop then
+        term.clear()
+        term.setCursorPos(0,0)
+        term.write("Dumping bad items!")
 
-    for s=1,16 do
-        local item = turtle.getItemDetail(s)
-        if item then
-            for _,l in ipairs(listed) do
-                if string.find(item.name, l) then
+        for s=1,16 do
+            local item = turtle.getItemDetail(s)
+            if item then
+                if not string.find(item.name,"raw") and not string.find("Redstone Dust Lapis Lazuli Diamond",item.displayName) then
                     turtle.select(s)
                     turtle.dropDown()
                 end
             end
         end
-    end]]
-    
+        term.clear()
+    end
 end
